d37005a7055ddf1d53a47086698e42c999f5959d
Joyce Wambui <jwambui@protonmail.com>
2022-01-24 18:23:16 +0300

    format files with black

--- dansemacabre/legrandmacabre/apps.py
+++ dansemacabre/legrandmacabre/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class LegrandmacabreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'legrandmacabre'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "legrandmacabre"
