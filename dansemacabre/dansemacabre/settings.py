ed111e565d93e16cf0bc5edfa83aaba6eaf7be68
Joyce Wambui <jwambui@protonmail.com>
2022-01-27 01:14:48 +0300

    set config vars on heroku, move .travis.yml and Procfile to project root

--- dansemacabre/dansemacabre/settings.py
+++ dansemacabre/dansemacabre/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import dj_database_url
 from decouple import config
+import django_on_heroku
+django_on_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
